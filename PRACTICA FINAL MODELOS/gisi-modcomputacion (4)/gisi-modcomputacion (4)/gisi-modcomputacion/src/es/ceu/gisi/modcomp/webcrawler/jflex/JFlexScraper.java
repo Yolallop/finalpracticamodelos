6bfcafac28425fe9022c2a1bd00af928ba19d64c
yolanda llop <yolanda llop@DESKTOP-VMKFV1D>
2021-04-28 11:39:46 +0200

    caso 0 y caso 1 HTML Y HEAD

--- PRACTICA FINAL MODELOS/gisi-modcomputacion (4)/gisi-modcomputacion (4)/gisi-modcomputacion/src/es/ceu/gisi/modcomp/webcrawler/jflex/JFlexScraper.java
+++ PRACTICA FINAL MODELOS/gisi-modcomputacion (4)/gisi-modcomputacion (4)/gisi-modcomputacion/src/es/ceu/gisi/modcomp/webcrawler/jflex/JFlexScraper.java
@@ -1,5 +1,6 @@
 package es.ceu.gisi.modcomp.webcrawler.jflex;
 
+import es.ceu.gisi.modcomp.webcrawler.jflex.lexico.Token;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,10 +16,39 @@ import java.util.ArrayList;
 public class JFlexScraper {
 
     HTMLParser analizador;
-
+    Stack <String> lecturasDePilas;
     public JFlexScraper(File fichero) throws FileNotFoundException {
         Reader reader = new BufferedReader(new FileReader(fichero));
         analizador = new HTMLParser(reader);
+        
+        int state = 0;
+        boolean etiquetaA = false; 
+        boolean etiquetaImg =  false;
+        Token tokenLeer= analizador.nextToken();
+        while (tokenLeer != null){
+            switch (state){
+                case 0 :
+                    if (tokenLeer.getTipo() == Tipo.OPEN){
+                        {
+                            state = 1;
+                          
+                                 
+                        }
+                        break; 
+                 case 1 : // palabra "HEAD 
+                    if (tokenLeer.getTipo() ==Tipo.PALABRA);
+                    lecturasDePila.add (tokenLeer.getValor());
+                    if (tokenleer.getValor().equalsIgnoreCase("a")){
+                        etiquetaA = true;
+                        
+                    }
+                    state = 2;
+                    }
+                
+            }
+        }
+        
+                
     }
 
     // Esta clase debe contener tu automata programado...
