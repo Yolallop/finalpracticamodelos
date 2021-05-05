bb6fb3ded6dab895fe966f050b1d245f31d75819
yolanda llop <yolanda llop@DESKTOP-VMKFV1D>
2021-05-05 11:06:39 +0200

    Crear metodo hiperenlacesImg, igual que hiperenlacesA pero con otros nombres

--- PRACTICA FINAL MODELOS/gisi-modcomputacion (4)/gisi-modcomputacion (4)/gisi-modcomputacion/src/es/ceu/gisi/modcomp/webcrawler/jflex/JFlexScraper.java
+++ PRACTICA FINAL MODELOS/gisi-modcomputacion (4)/gisi-modcomputacion (4)/gisi-modcomputacion/src/es/ceu/gisi/modcomp/webcrawler/jflex/JFlexScraper.java
@@ -161,7 +161,20 @@ public class JFlexScraper {
        
     
 
-    public ArrayList<String> obtenerHiperenlacesImagenes() {
+    public ArrayList<String> obtenerHiperenlacesImagenes() throws IOException {
+           File crearEnlacesA = new File ("obtenerHiperenlacesIMG.txt");
+        FileWriter fileEnlacesA =  new FileWriter ("obtenerHiperenalcesIMG.txt"); // se escribe 
+        BufferedWriter escribirEnlacesA = new BufferedWriter(fileEnlacesA); // PASO AL FICHERO ARRAY LIST 
+       
+
+        for ( int i=0;i<enlacesImg.size();i++){
+        escribirEnlacesA.write(enlacesImg.get(i));
+        escribirEnlacesA.newLine();
+        }
+        escribirEnlacesA.close();
+        return enlacesImg;  
+
+        
         
     }
 
